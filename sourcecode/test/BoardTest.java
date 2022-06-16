7d77e18d233ef14c3e1f4e176d19d9222d44b049
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2022-06-16 15:11:56 +0700

    upate BoardTest

--- sourcecode/test/BoardTest.java
+++ sourcecode/test/BoardTest.java
@@ -1,4 +1,16 @@
 package sourcecode.test;
 
+import sourcecode.board.Board;
+import sourcecode.board.Cell;
+
 public class BoardTest {
+    public static void main(String[] args) {
+        Board newBoard = new Board();
+        Cell[] board = newBoard.getBoard();
+
+        for(Cell cell:board) {
+            System.out.println(cell);
+        }
+    }
+
 }
