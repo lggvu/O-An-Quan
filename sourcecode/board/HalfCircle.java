855dafd2478186f74da1584e0569dbf623ac330e
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2022-06-17 14:33:45 +0700

    update class diagram

--- sourcecode/board/HalfCircle.java
+++ sourcecode/board/HalfCircle.java
@@ -3,13 +3,13 @@ package sourcecode.board;
 import sourcecode.board.Cell;
 
 public class HalfCircle extends Cell {
-    private boolean bigGem;
+    private boolean hasBigGem;
 
     public HalfCircle(int position, int numberOfGems) {
         super(position, numberOfGems);
     }
 
-    public boolean isBigGem() {
-        return bigGem;
+    public boolean hasBigGem() {
+        return hasBigGem;
     }
 }
