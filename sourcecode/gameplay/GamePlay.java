bdb0057986271a21434d0b43699dfbd3e6606e28
PhamVuHuyenTrang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2022-06-21 15:16:54 +0700

    Update GamePlay.java

--- sourcecode/gameplay/GamePlay.java
+++ sourcecode/gameplay/GamePlay.java
@@ -1,27 +1,28 @@
-package sourcecode.gameplay;
+package sourcecode.player;
 
-import sourcecode.board.Board;
-import sourcecode.player.Player;
+import sourcecode.board.Cell;
 
-public class GamePlay {
-    public static void main(String[] args) {
-        Board board = new Board();
-        Player player1 = new Player("Player 1");
-        Player player2 = new Player("Player 2");
-        player1.setTurn(Math.random() < 0.5);
-        if (math.random < 0.5){
-            GameStartWithPlayer1();
-        }
+public class Hand {
+    private Cell handPosition;
+    private int direction;
+    /*
+    direction = 0: clockwise
+    direction = 1: anti-clockwise
+     */
+
+    public void setHandPosition(Cell handPosition) {
+        this.handPosition = handPosition;
     }
 
-    public static void GameStartWithPlayer1(){
-        while(player1.pickUpGemFrom(new Cell((int)(math.ramdom() * 11))) == false){
-            System.out.printline("Pick cell from another Cell");
-        }
-
-
+    public Cell getHandPosition() {
+        return handPosition;
     }
 
+    public int getDirection() {
+        return direction;
+    }
 
-
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
 }
