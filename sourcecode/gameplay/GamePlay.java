ee538607006474cc58d78248ff9c2f349bf520af
PhamVuHuyenTrang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2022-06-30 23:35:57 +0700

    Complete GamePlay

--- sourcecode/gameplay/GamePlay.java
+++ sourcecode/gameplay/GamePlay.java
@@ -16,76 +16,77 @@ public class GamePlay {
         System.out.println("Introduction");
         Player1.setTurn(Math.random() < 0.5);
         if (Player1.isInTurn()){
+        	System.out.println("Player 1 plays first");
             GameStart(Player1, Player2, board);
         } else {
             GameStart(Player2, Player1, board);
+            System.out.println("Player 2 plays first");
         }
     }
+    public static boolean isGameOver(Player player1, Player player2, Board board) {
+    	boolean res = false;
+    	if (board.getNumBigGem() == 0) {
+    		res = true;
+    	}
+    	return res;
+    }
     public static void GameStart(Player player1, Player player2, Board board){
-        int Player1NumOfBorrow = 0;
-        int Player2NumOfBorrow = 0;
         ArrayList <Cell> Player1CellOnSide = new ArrayList<Cell>();
         ArrayList <Cell> Player2CellOnSide = new ArrayList<Cell>();
-        for (int i = 1; i < 6; i ++) {  // TODO generalise these quantities
-            Player1CellOnSide.add(new Cell(i));
+        Cell[] boardList = board.getBoard();
+        for (int i = 1; i < 6; i ++) { 
+            Player1CellOnSide.add(boardList[i]);
         }
-        for (int i = 8; i < 12; i ++) {
-            Player2CellOnSide.add(new Cell(i));
+        for (int i = 7; i < 12; i ++) {
+            Player2CellOnSide.add(boardList[i]);
         }
+        player1.setCellsOnSide(Player1CellOnSide);
+        player2.setCellsOnSide(Player2CellOnSide);
 
-        while ((!(board.getNumBigGem() == 0 && board.getNumSmallGem() == 0)) || (!(board.getBoard()[0]).isEmpty() && board.getBoard()[board.getNumSquare() / 2 + board.getNumHalfCircle() / 2].isEmpty())) {
-            if (!player1.isCellOnSideEmpty()) {
-                while(!player1.pickUpGemFrom(new Cell((int)(Math.random() * 11))) ){
-                    System.out.print("Pick gem from another Cell");
-                }
-                if(player1.pickUpGemFrom(new Cell((int)(Math.random() * 11)))) {
-                    System.out.println("Choose Hand Direction");
-                    int Player1HandDirection = sc.nextInt();
-                    player1.spreadGem(new Cell((int)(Math.random() * 11)), Player1HandDirection, board);
-                    System.out.println("Player 2's turn");
-                    if (!player2.isCellOnSideEmpty()) {
-                        while(!player2.pickUpGemFrom(new Cell((int)(Math.random() * 11))) ){
-                            System.out.println("Pick gem from another Cell");
-                        }
-                        if(player2.pickUpGemFrom(new Cell((int)(Math.random() * 11)))){
-                            System.out.println("Choose Hand Direction");
-                            int Player2HandDirection = sc.nextInt();
-                            player2.spreadGem(new Cell((int)(Math.random() * 11)), Player2HandDirection, board);
-                        }
-                    } else {
-                        player2.setBorrow(true);
-                        Player2NumOfBorrow += 1;
-                        player2.setNumBorrow(5 * Player2NumOfBorrow);
-                        player2.setNumBorrow(5 * Player2NumOfBorrow);
-                        // TODO add five small gems to gemInHand of player 2??
-                        while(!player2.pickUpGemFrom(new Cell((int)(Math.random() * 11))) ){
-                            System.out.println("Pick gem from another Cell");
-                        }
-                        if(player2.pickUpGemFrom(new Cell((int)(Math.random() * 11)))){
-                            System.out.println("Choose Hand Direction");
-                            int Player2HandDirection = sc.nextInt();
-                            player2.spreadGem(new Cell((int)(Math.random() * 11)), Player2HandDirection, board);
-                        }
-                    }
-                }
+        while (!(isGameOver(player1, player2, board))) {
+        	System.out.println("Player 1's turn");
+        	System.out.println("*********************************************");
+        	System.out.println("Player 1 chooses a cell to pick gems from: ");
+        	int cell_Num = sc.nextInt();
+        	sc.nextLine();
+            while (!(player1.pickUpGemFrom(boardList[cell_Num]))){
+                System.out.println("The cell is invalid. Please choose another cell to pick!");
+                cell_Num = sc.nextInt();
+                sc.nextLine();
             }
-            else {
-                // Borrow Gem from the other player
-                player1.setBorrow(true);
-                Player1NumOfBorrow += 1;
-                player1.setNumBorrow(5 * Player1NumOfBorrow);
-                // TODO add five small gems to gemsInHand of player 1??
-                GameStart(player1, player2, board);
-                // TODO If GameStart(player1, player2, board) is used, can we preserve the number of gems in hand,... of 2 players?
+            System.out.println("Choose Hand Direction");
+            int Player1HandDirection = sc.nextInt();
+            sc.nextLine();
+            while (Player1HandDirection != 1 && Player1HandDirection != 0) {
+                System.out.println("Invalid Hand Direction. Please choose again!");
             }
+            player1.spreadGem(boardList[cell_Num], Player1HandDirection, board);
+            System.out.println("*****************************************");
+            System.out.println("Player 2's turn");
+            System.out.println("Player 2 chooses a cell to pick gems from: ");
+            int cell_Num2 = sc.nextInt();
+            sc.nextLine();
+            while(!player2.pickUpGemFrom(boardList[cell_Num2] )){
+                System.out.println("The cell is invalid. Please choose another cell to pick!");
+                sc.nextLine();
+                }
+            player2.pickUpGemFrom(boardList[cell_Num2]);
+            System.out.println("Choose Hand Direction");
+            int Player2HandDirection = sc.nextInt();
+            while (Player2HandDirection != 1 && Player2HandDirection != 0) {
+                System.out.println("Invalid Hand Direction. Please choose again!");
+            }
+            player2.spreadGem(boardList[cell_Num2], Player2HandDirection, board);
+   
         }
         if (player1.calculateScore() > player2.calculateScore()) {
-            System.out.println(player1 + " wins.");
+            System.out.println(player1.getName() + " wins.");
         } else if (player1.calculateScore() < player2.calculateScore()) {
-            System.out.println(player2 + " wins.");
+            System.out.println(player2.getName() + " wins.");
         } else {
             System.out.println("Draw");
         }
+        System.exit(0);
 
     }
 }
