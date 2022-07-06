7a3a503cba3583b239c24abad17a4e001380bea0
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2022-07-06 16:13:27 +0700

    update gui

---
+++ sourcecode/screen/controller/GamePlayScreenController.java
@@ -0,0 +1,151 @@
+package sourcecode.screen.controller;
+
+import javafx.fxml.FXML;
+        import javafx.scene.control.Label;
+        import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class GamePlayScreenController {
+
+    @FXML
+    private Pane cell1;
+
+    @FXML
+    private ImageView leftArrowCell1;
+
+    @FXML
+    private ImageView rightArrowCell1;
+
+    @FXML
+    private Label numOfGems1;
+
+    @FXML
+    private Pane cell3;
+
+    @FXML
+    private ImageView leftArrowCell3;
+
+    @FXML
+    private ImageView rightArrowCell3;
+
+    @FXML
+    private Label numOfGems3;
+
+    @FXML
+    private Pane cell4;
+
+    @FXML
+    private ImageView rightArrowCell4;
+
+    @FXML
+    private Label numOfGems4;
+
+    @FXML
+    private Pane cell5;
+
+    @FXML
+    private ImageView leftArrowCell5;
+
+    @FXML
+    private ImageView rightArrowCell5;
+
+    @FXML
+    private Label numOfGems5;
+
+    @FXML
+    private Pane cell7;
+
+    @FXML
+    private ImageView leftArrowCell7;
+
+    @FXML
+    private ImageView rightArrowCell7;
+
+    @FXML
+    private Label numOfGems7;
+
+    @FXML
+    private Pane cell8;
+
+    @FXML
+    private ImageView leftArrowCell8;
+
+    @FXML
+    private ImageView rightArrowCell8;
+
+    @FXML
+    private Label numOfGems8;
+
+    @FXML
+    private Pane cell9;
+
+    @FXML
+    private ImageView leftArrowCell9;
+
+    @FXML
+    private ImageView rightArrowCell9;
+
+    @FXML
+    private Label numOfGems9;
+
+    @FXML
+    private Pane cell10;
+
+    @FXML
+    private ImageView leftArrowCell10;
+
+    @FXML
+    private ImageView rightArrowCell10;
+
+    @FXML
+    private Label numOfGems10;
+
+    @FXML
+    private Pane cell11;
+
+    @FXML
+    private ImageView leftArrowCell11;
+
+    @FXML
+    private ImageView rightArrowCell11;
+
+    @FXML
+    private Label numOfGems11;
+
+    @FXML
+    private Pane cell2;
+
+    @FXML
+    private Label numOfGems2;
+
+    @FXML
+    private Label numOfGems12;
+
+    @FXML
+    private Label player1Name;
+
+    @FXML
+    private Label player2Name;
+
+    @FXML
+    void cellChosen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void leftDirectionChosen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void rightArrowChosen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void rightDirectionChosen(MouseEvent event) {
+
+    }
+
+}
