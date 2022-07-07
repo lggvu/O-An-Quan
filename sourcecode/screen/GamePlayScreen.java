2aaef47423ff064fabc24e157bb992e1c5f47aed
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2022-07-07 16:19:19 +0700

    change gui structure

--- sourcecode/screen/GamePlayScreen.java
+++ sourcecode/screen/GamePlayScreen.java
@@ -1,30 +1,44 @@
 package sourcecode.screen;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import sourcecode.board.Board;
+import sourcecode.player.Player;
+import sourcecode.screen.controller.GamePlayScreenController;
+
+import java.io.IOException;
 
 
 public class GamePlayScreen extends Application {
+    private static Board board;
+    private static Player player1;
+    private static Player player2;
 
     public void start(Stage primaryStage)
     {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 650, 650);
-        Image img = new Image("sourcecode/screen/images/background.png");
-        BackgroundImage bImg = new BackgroundImage(img,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
-        Background bGround = new Background(bImg);
-        root.setBackground(bGround);
-        primaryStage.setScene(scene);
+        final String STORE_FXML_FILE_PATH = "/sourcecode/screen/view/PlayScreen.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(STORE_FXML_FILE_PATH));
+        GamePlayScreenController viewStoreController = new GamePlayScreenController(board, player1, player2);
+        fxmlLoader.setController(viewStoreController);
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        primaryStage.setTitle("O An Quan");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
     public static void main(String[] args) {
+        board = new Board();
+        player1 = new Player("Player 1");
+        player2 = new Player("Player 2");
+
         launch(args);
     }
 
